7b6f2cbdaac956ede5ca735ecbc989f4d97205e6
Kshilinaa <ksusha_2605@mail.ru>
2024-02-06 13:48:52 +0300

    Установила swiftformat и swiftlint

---
+++ UIKitDz/ViewController.swift
@@ -0,0 +1,12 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Class Controller
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}

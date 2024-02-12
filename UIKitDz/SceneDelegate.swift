97dfe9fa3fd25c0dad392c040edc6584d94f093f
Kshilinaa <ksusha_2605@mail.ru>
2024-02-12 10:42:21 +0300

    Выполнила задание UIPickerView & DatePicker

--- UIKitDz/SceneDelegate.swift
+++ UIKitDz/SceneDelegate.swift
@@ -6,22 +6,23 @@ import UIKit
 /// SceneDelegate Class
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    var navigationController: UINavigationController?
 
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        ///        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let loginVC = RegisterViewController()
+        let navigationVC = UINavigationController()
+
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = navigationVC
+        navigationVC.pushViewController(loginVC, animated: true)
+        window.makeKeyAndVisible()
+        self.window = window
+        window.backgroundColor = .white
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {}
-
-    func sceneDidBecomeActive(_ scene: UIScene) {}
-
-    func sceneWillResignActive(_ scene: UIScene) {}
-
-    func sceneWillEnterForeground(_ scene: UIScene) {}
-
-    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
